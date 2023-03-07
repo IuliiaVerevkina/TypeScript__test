ebea97b3b6c166f4f1ccc797063918827f007433
Iuliia <iulia.verevkina@mail.ru>
2023-03-07 13:06:12 +0100

    edit user interface

--- 4-dummy-api/tupings_4.ts
+++ 4-dummy-api/tupings_4.ts
@@ -14,68 +14,66 @@ enum Gender {
   Female = "female",
 }
 
-interface User {
-  users: [
-    {
-      id: number;
-      firstName: string;
-      lastName: string;
-      maidenName?: string;
-      age: number;
-      gender: Gender;
-      email: string;
-      phone: string;
-      username: string;
-      password: string;
-      birthDate: string;
-      image: string;
-      bloodGroup: string;
-      height: number;
-      weight: number;
-      eyeColor: string;
-      hair: {
-        color: string;
-        type: string;
-      };
-      domain: string;
-      ip: string;
-      address: {
-        address: string;
-        city: string;
-      };
-      macAddress: string;
-      university: string;
-      bank: {
-        cardExpire: string;
-        cardNumber: string;
-        cardType: string;
-        currency: string;
-        iban: string;
-      };
-      company: {
-        address: {
-          address: string;
-          city: string;
-          coordinates: {
-            lat: number;
-            lng: number;
-          };
-          postalCode: string;
-          state: string;
-        };
-        department: string;
-        name: string;
-        title: string;
-      };
-      ein: string;
-      ssn: string;
-      userAgent: string;
-    }
-  ];
+interface UsersCountInfo {
   total: number;
   skip: number;
   limit: number;
-  
-};
+  users: CompleteInfoUser;
+}
 
+interface CompleteInfoUser extends User, adressCompany{}
 
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  maidenName?: string;
+  age: number;
+  gender: Gender;
+  email: string;
+  phone: string;
+  username: string;
+  password: string;
+  birthDate: string;
+  image: string;
+  bloodGroup: string;
+  height: number;
+  weight: number;
+  eyeColor: string;
+  hair: {
+    color: string;
+    type: string;
+  };
+  domain: string;
+  ip: string;
+  addressHome: {
+    address: string;
+    city: string;
+  };
+  macAddress: string;
+  university: string;
+  bank: {
+    cardExpire: string;
+    cardNumber: string;
+    cardType: string;
+    currency: string;
+    iban: string;
+  };
+  ein: string;
+  ssn: string;
+  userAgent: string;
+}
+
+interface adressCompany {
+  addressCompany: string;
+  city: string;
+  coordinates: {
+    lat: number;
+    lng: number;
+  };
+  postalCode: string;
+  state: string;
+  department: string;
+  name: string;
+  title: string;
+}
